c1b940cb5acdf1dcb454f95ace3fc523677457e6
elmaccho <maciek.chojnacki22@wp.pl>
2023-08-11 22:18:34 +0200

    404 page

--- 404.php
+++ 404.php
@@ -1,1 +1,6 @@
-pojebalo nie ma takiej strony
+<div class="error404">
+    <p>404</p>
+    <span>Ups... Nie ma takiej strony</span><br>
+
+    <a href="./startindex.php" class="returnBtn">Powrót do strony głównej</a>
+</div>
